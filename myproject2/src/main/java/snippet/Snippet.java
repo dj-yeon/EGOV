abe3ae42b17e99734e2e144ba72daa9be15bc137
aplusit <aplusit@DESKTOP-R2020H0>
2022-06-10 10:56:33 +0900

    공유

---
+++ myproject2/src/main/java/snippet/Snippet.java
@@ -0,0 +1,17 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		<dependency>
+		       <groupId>com.oracle</groupId>
+		       <artifactId>ojdbc6</artifactId>
+		       <version>11.2.0.3</version>
+		</dependency> 
+		<dependency>
+		        <groupId>commons-dbcp</groupId>
+		        <artifactId>commons-dbcp</artifactId>
+		        <version>1.4</version>
+		</dependency>
+	}
+}
+
